a9127c5bb631fd59df18e68ec4f9080ad6f325ea
Micky Woo <mwoo@seattleu.edu>
2020-04-28 17:25:01 -0700

    first commit

---
+++ src/views/Reverse.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="reverse">
+    <h1>This is a reverse page</h1>
+
+    <h2> please enter a string to reverse!</h2>
+    <label> String:
+      <input type="text" id="string" v-model="message">
+      </label>
+      <h2> the reverse message is:  </h2>
+      <p> {{ reverseMessage}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'reverse',
+  data() {
+    return {
+      message: 'hello'
+    }
+  },
+  computed: {
+    reverseMessage() {
+      return this.message.split('').reverse().join('');
+    }
+  }
+}
+</script>
